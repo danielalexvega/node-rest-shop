8086f853730cb9727cbdc0727f665f58db4a3f6c
Daniel Vega <daniel.vega@austinisd.org>
2020-10-14 15:53:20 -0500

    add body-parser

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 app.use((rew, res, next) => {
     const error = new Error('Not found');
-    error.status(404);
+    error.status = 404;
     next(error);
 });
 
