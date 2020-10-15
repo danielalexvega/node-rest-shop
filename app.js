af199e6bf61e7cd7c3851c04709dd3beed519cb0
danielalexvega <danielalexvega@gmail.com>
2020-10-15 13:26:42 -0500

    add product schema

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://dbAdmin:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop.t7ry1.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+        useMongoClient: true
+    }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
@@ -16,10 +23,11 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
-    if(req.method === 'OPTIONS') {
+    if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
+    next();
 });
 
 // Routes which should handle requests
